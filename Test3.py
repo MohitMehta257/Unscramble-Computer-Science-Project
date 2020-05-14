4df9b355f696d5c993f797ecbbe31be801b7ff53
MohitMehta257 <58682658+MohitMehta257@users.noreply.github.com>
2020-05-14 23:50:34 +0530

    Update Test3.py

--- Test3.py
+++ Test3.py
@@ -3,52 +3,55 @@ import csv
 with open('calls.csv','r') as f:
     data=csv.reader(f)
     text=list(data)
-l=[]
+calls=[]
 for i in range(len(text)):
-    l.append(text[i][1])
+    calls.append(text[i][0])
+calls1=[] 
+for i in range(len(text)):
+    calls1.append(text[i][1])
+bang=[]
 
-ban=[]
-for i in range(len(l)):
-    if(l[i].count("(080)")==1):
-        ban.append(l[i])
+for each in calls:
+    if each.count("080")==1:
+        bang.append(each)
 
-index=[]
-for i in range(len(ban)):
-    index.append(l.index(ban[i]))
+rece=[]
+for each in bang:
+    rece.append(calls1[calls.index(each)])
 
-call=[]
-for each in index:
-    call.append(text[each][0])
-
-code=[]
-for each in call:
-    if(each[0]=='('):
-        t=each.index(')')
-        code.append(each[1:t])
-    if(each.count("140")==1):
-        code.append(each[0:3])
+bangalore=[]
+for each in rece:
+    if each[0]=="(":
+        index=each.index(")")
+        bangalore.append(each[0:index+1])
+    if each[0]=="140":
+        bangalore.append(each[0:3])
     else:
-        code.append(each[0:4])
-code1=[]
-     
-for each in code:
-    if(each[0]=='('):
-        code1.append(each[1:])
-    else:
-        code1.append(each)
+        bangalore.append(each[0:4])
+        
+
+bangalore2=[]
+for each in bangalore:
+    if bangalore2.count(each)==0:
+        bangalore2.append(each)
+        
+
+
+
+numerator=0
+for i in range(len(text)):
+    if(calls[i].count("080")==1 and calls1[i].count("080")==1):
+        numerator+=1
+
+denum=0
+
+for each in calls:
+    if(each.count("080")==1):
+        denum+=1
  
-
-code2=[]
-for each in code1:
-    if(code2.count(each)==0):
-        code2.append(each)
-
-print("The numbers called by people in Bangalore have codes:",sorted(code2))
-n=len(sorted(code2))
-
-bang=code2.count("080")
-
-per=(bang/n)*100
-print("{:.2f}".format(per),"percent of calls from fixed lines in Bangalore are calls to other fixed lines in Bangalore.")
-
- 
+per=(numerator/denum)*100
+   
+print("The numbers called by people in Bangalore have codes:",sorted(bangalore2))
+print("{:.2f}".format(per),"percent of calls from fixed lines in Bangalore are calls to other fixed lines in Bangalore.")       
+        
+        
